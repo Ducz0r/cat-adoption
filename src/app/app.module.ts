12ab51ec9a167bbaeb82f73b30440b2807fc5c14
Luka Murn <murn.luka@gmail.com>
2024-04-16 15:36:17 +0200

    Add cats module, move navbar to layout folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './layout/components';
 
 @NgModule({
   declarations: [
