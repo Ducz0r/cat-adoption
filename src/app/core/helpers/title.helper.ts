9d589bdc3a1ea5baf45413f21b18c9882b8d74dc
Luka Murn <murn.luka@gmail.com>
2024-04-19 14:39:48 +0200

    Add sign in form

--- src/app/core/helpers/title.helper.ts
+++ src/app/core/helpers/title.helper.ts
@@ -1,5 +1,5 @@
 export class TitleHelper {
-  private static readonly titlePrefix: string = 'Cats Adoption';
+  private static readonly titlePrefix: string = 'Cat Adoption';
   private static readonly titleDelimiter: string = ' | ';
 
   public static genTitle(...args: string[]): string {
